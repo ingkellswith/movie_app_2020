9e73d5f1b40c10f07cfda3722b98b7805f994e9f
ingkellswith <allover3773@gmail.com>
2020-11-17 20:26:10 +0900

    bonus lecture was done

---
+++ src/routes/Detail.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  } //render후에 componentDidMount가 실행됨
+}
+export default Detail;
